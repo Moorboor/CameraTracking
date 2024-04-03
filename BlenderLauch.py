0f85176026636cd4d775772da16c31c9e4266d78
MoorboorZenBook <moorboory@gmail.com>
2024-04-03 09:36:52 +0200

    Blender script

--- BlenderLauch.py
+++ BlenderLauch.py
@@ -1,9 +1,13 @@
 import subprocess
+import os
+
+ABS_PATH = os.path.abspath("")
+
 
 
 def render_blender():
     blender_executable = r"C:\Program Files\Blender Foundation\Blender 4.0\blender-launcher.exe"
-    blender_script = r"C:\Users\moorb\OneDrive\Documents\MyProjects\Code\Repositories\CameraTracking\blender_script.py"
+    blender_script = os.path.join(ABS_PATH, f"blender_script.py")
 
     # command = [blender_executable]
     command = [blender_executable, "--background", "--python", blender_script]
--- blender_script.py
+++ blender_script.py
@@ -1,15 +1,16 @@
-#import bpy
+import bpy
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
 
 ABS_PATH = os.path.abspath("")
 RENDER_PATH = os.path.join(ABS_PATH, "Renders")
 INPUT_PATH = os.path.join(ABS_PATH, "2023", "12", "01")
 os.makedirs(RENDER_PATH, exist_ok=True)
 
-utc_time = datetime.utcnow()
-date_string = utc_time.strftime('%Y-%m-%d')
 
+
+def move_objects():
+    for obj in bpy.data.objects: print(obj.name)
 
 
 def render_video(output_path):
@@ -23,13 +24,15 @@ def render_video(output_path):
 
     # Set frame range
     bpy.context.scene.frame_start = 1
-    bpy.context.scene.frame_end = 250  # Set your desired end frame
+    bpy.context.scene.frame_end = 10  # Set your desired end frame
 
     # Render animation
     bpy.ops.render.render(animation=True)
     return True
 
 video_names = os.listdir(INPUT_PATH)
-print(video_names)
+video_path = r"C:\Users\moorb\OneDrive\Desktop\PXL_20240327_121126161.mp4"
 
-# render_video(os.path.join(RENDER_PATH, f"{date_string}.mp4"))
+
+# render_video(os.path.join(RENDER_PATH, f"test.mp4"))
+move_objects()
